a03f8c1252d0aba0cc44002b137b668393bc470f
Andrew Zong <a.zong@f5.com>
2019-12-16 09:49:15 +0800

    fix md5 bin issue -q or --quiet

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -6,15 +6,15 @@ export HOMEDIR=$cdir/..
 
 function refresh_image_if_necessary() {
     
-    md5bin=`which md5`
-    if [ x"$md5bin" = x ]; then md5bin=`which md5sum`; fi; 
-    
+    md5bin=`which md5`" -q"
+    if [ x"$md5bin" = x" -q" ]; then md5bin=`which md5sum`" --quiet"; fi; 
+
     for n in fluentd sandbox; do 
         echo generating image: $n; 
 
         dockerfile_path=$HOMEDIR/docker/$n/Dockerfile
         md5_file=$HOMEDIR/docker/.$n.image.md5
-        curmd5=`$md5bin -q $dockerfile_path`
+        curmd5=`$md5bin $dockerfile_path`
         image_name=zongzw/$n:latest
         image_found=`docker images --format "{{.Repository}}:{{.Tag}}" | grep $image_name`
 
@@ -22,7 +22,7 @@ function refresh_image_if_necessary() {
             -o "`cat $md5_file`" != "$curmd5" \
             -o x"$image_found" = x ]; then
             docker build -t $image_name $(dirname $dockerfile_path);
-            $md5bin -q $dockerfile_path > $md5_file;
+            $md5bin $dockerfile_path > $md5_file;
         fi
     done
     
