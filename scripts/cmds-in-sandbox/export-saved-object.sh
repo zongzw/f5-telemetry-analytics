8c7d4575df06f94fd598ff981981dd0cab7961b7
Andrew Zong <a.zong@f5.com>
2019-12-17 18:40:54 +0800

    export dashboard data.

--- scripts/cmds-in-sandbox/export-saved-object.sh
+++ scripts/cmds-in-sandbox/export-saved-object.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-#curl 
+if [ $# -ne 2 ]; then 
+    echo "$0 <dashboard_id> <target_directory>"
+    exit 1
+fi
+
+curl -XGET \
+    http://kibana:5601/api/kibana/dashboards/export?dashboard=$1 \
+    -o $2/$1.json
+    
